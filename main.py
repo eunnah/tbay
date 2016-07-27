2b496edbf27257bae295a42c103c4d8f7381f65e
eunnah <eunnah@gmail.com>
2016-07-27 05:34:51 +0000

    finished tbay.py

--- main.py
+++ main.py
@@ -3,37 +3,67 @@ from tbay import User, Item, Bid, session
 beyonce = User()
 beyonce.username = "bknowles"
 beyonce.password = "uhohuhohuhohohnana"
+beyonce.id = "78"
 session.add(beyonce)
 session.commit()
 
 jayz = User()
 jayz.username = "jayz"
 jayz.password = "99problems"
+jayz.id = "56"
 session.add(jayz)
+session.commit()
+
+solange = User()
+solange.username = "solange"
+solange.password = "sister"
+solange.id = "1234"
+session.add(solange)
 session.commit()
 
 crown = Item()
 crown.name = "crown"
 crown.description = "a beautiful crown"
+crown.seller_id = solange.id
 session.add(crown)
 session.commit()
 
-shoe = Item()
-shoe.name = "shoe"
-shoe.description = "a beautiful golden shoe"
-session.add(shoe)
+baseball = Item()
+baseball.name = "baseball"
+baseball.description = "a beautiful golden baseball"
+baseball.seller_id = solange.id
+session.add(baseball)
 session.commit()
 
-user = session.query(User).first()
-user.username = "solange"
+jayzbid = Bid()
+jayzbid.price = 3
+jayzbid.bidder_id = jayz.id
+jayzbid.bidded_item = baseball.id
+session.add(jayzbid)
 session.commit()
 
-item = session.query(Item).first()
-item.name = "piggybank"
+beyoncebid = Bid()
+beyoncebid.price = 5
+beyoncebid.bidder_id = beyonce.id
+beyoncebid.bidded_item = baseball.id
+session.add(beyoncebid)
 session.commit()
 
-item = session.query(Item).get(2)
-item.name = "toothbrush"
+solange.auctioned_items.append = baseball 
+jayz.bids_made.append = jayzbid
+beyonce.bids_made.append = beyoncebid
+baseball.bids_received.append(beyoncebid)
+baseball.bids_received.append(jayzbid)
+session.commit()
+
+#Perform a query to find out which user placed the highest bid
+if beyoncebid.price > jayzbid.price:
+    print("Beyonce wins the item!")
+elif beyoncebid.price == jayzbid.price:
+    print("It's a tie!")
+else:
+    print("Jayz wins the item!")
+    
 session.commit()
 
 # do you have to commit one at a time? http://stackoverflow.com/questions/974596/what-is-a-database-transaction/974611#974611
@@ -41,26 +71,20 @@ session.commit()
 # Returns a list of all of the user objects
 # Note that user objects won't display very prettily by default -
 # you'll see their type (User) and their internal identifiers.
-print(session.query(User).all()) # Returns a list of all of the user objects
+#print(session.query(User).all()) # Returns a list of all of the user objects
 
 # Returns the first user
-print(session.query(User).first())
+#print(session.query(User).first())
 
 # Returns the first item
-print(session.query(Item).first())
+#print(session.query(Item).first())
 
 # Finds the user with the primary key equal to 1
-print(session.query(User).get(1))
+#print(session.query(User).get(1))
 
 # Returns a list of all of the usernames in ascending order
-print(session.query(User.username).order_by(User.username).all())
+#print(session.query(User.username).order_by(User.username).all())
 
 # Returns a list of all of the items
-print(session.query(Item.name).order_by(Item.name).all())
+#print(session.query(Item.name).order_by(Item.name).all())
 
-
-session.query(User).delete()
-session.commit()
-
-session.query(Item).delete()
-session.commit()
--- tbay.py
+++ tbay.py
@@ -1,16 +1,19 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-
+from sqlalchemy import Table, Column, Integer, String, ForeignKey, DateTime, Float
+from sqlalchemy.orm import relationship
+from datetime import datetime
 
 engine = create_engine('postgresql://ubuntu:thinkful@localhost:5432/tbay')
 Session = sessionmaker(bind=engine)
 session = Session()
 Base = declarative_base()
 
-from datetime import datetime
 
-from sqlalchemy import Column, Integer, String, DateTime, Float
+# One to many relationship between item and bids (item can have multiple bids)
+# One to many relationship between user and bids (user can have multiple bids)
+# One to many relationship between user and auctioning items (user can auction multiple items)
 
 class Item(Base):
     __tablename__ = "items"
@@ -19,6 +22,8 @@ class Item(Base):
     name = Column(String, nullable=False)
     description = Column(String)
     start_time = Column(DateTime, default=datetime.utcnow)
+    seller_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    bids_received = relationship("Bid", backref="items")
     
 class User(Base):
     __tablename__ = "users"
@@ -26,13 +31,16 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     username = Column(String, nullable=False)
     password = Column(String, nullable=False)
+    auctioned_items = relationship("Item", backref="users")
+    bids_made = relationship("Bid", backref="users")
 
 class Bid(Base):
     __tablename__ = "bids"
 
     id = Column(Integer, primary_key=True)
     price = Column(Float, nullable=False)
+    bidder_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    bidded_item = Column(Integer, ForeignKey('items.id'), nullable=False)
 
-# Base.metadata.drop_all(engine) resets everything
 Base.metadata.drop_all(engine)
 Base.metadata.create_all(engine)
